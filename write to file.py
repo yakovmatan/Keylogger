29264b940180f014ef626bfc9941524e2e906b3e
shuki <shukiheftler@gmail.com>
2025-02-19 11:18:04 +0200

    write to file

---
+++ write to file.py
@@ -0,0 +1,33 @@
+import time
+import json
+from time import localtime
+class WriteToFile:
+    def __init__(self,data):
+        self.data = data
+        self.dic = {}
+    @staticmethod
+    def timer_hours():
+        return time.strftime('%H:%M', localtime())
+
+    @staticmethod
+    def timer_days():
+        return time.strftime('%d-%m-%y', localtime())
+
+    def enter_to_dic(self):
+        if self.timer_hours() in self.dic:
+            self.dic[self.timer_hours()] += self.data
+        else:
+            self.dic[self.timer_hours()] = self.data
+        return self.dic
+
+    def write_to_json_file(self):
+        json_data = json.dumps(self.enter_to_dic(), indent=4 , ensure_ascii=False)
+        with open(f"{self.timer_days()}.json", "w" ,encoding='utf-8') as file:
+            file.write(json_data)
+        self.dic.clear()
+    def write_to_file(self):
+        with open(f"{self.timer_days()}.txt", "w" , encoding='utf-8') as file:
+            file.write(f"   ***{self.timer_hours()}*** \n {self.data}\n")
+
+f = WriteToFile("shuki")
+f.write_to_json_file()
